f59ddfbaa2d7b67f40927f8f564bd38694a2b0d6
Surajbiswal001 <surajbiswal372@gmail.com>
2025-04-09 10:58:08 +0530

    Delete Project directory

--- Project/practice/3.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-
-with open('file.html', 'r') as f:
-    contents = f.read()
-
-    soup = BeautifulSoup(contents, "html.parser")
-
-    for tag in soup.find_all('li'):
-        print(soup.select('html head title'))
